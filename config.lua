2eeef669267c7630b04aa69d81d13727afa7e32b
Justin <justin@thinkingpandas.com>
2023-07-09 00:54:05 +0900

    feat: add more plugins

--- config.lua
+++ config.lua
@@ -4,6 +4,45 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 lvim.plugins = {
+  -- neo-scroll
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
+  {
+    "nacro90/numb.nvim",
+    event = "BufRead",
+    config = function()
+      require("numb").setup {
+        show_numbers = true,    -- Enable 'number' for the window while peeking
+        show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+      }
+    end,
+  },
+  -- {
+  --   'wfxr/minimap.vim',
+  --   build = "cargo install --locked code-minimap",
+  --   -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+  --   config = function()
+  --     vim.cmd("let g:minimap_width = 10")
+  --     vim.cmd("let g:minimap_auto_start = 1")
+  --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
+  --   end,
+  -- },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    lazy = true,
+    config = function()
+      require("persistence").setup {
+        dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+      }
+    end,
+  },
   -- trouble
   {
     "folke/trouble.nvim",
@@ -14,16 +53,17 @@ lvim.plugins = {
     cmd = "Copilot",
     event = "InsertEnter",
   },
+  -- multiple cursors
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+  },
   {
     "zbirenbaum/copilot-cmp",
     after = { "copilot.lua" },
     config = function()
       require("copilot_cmp").setup()
     end,
-  },
-  {
-    "justinmk/vim-sneak",
-    event = "BufRead",
   },
   {
     "kylechui/nvim-surround",
@@ -34,7 +74,15 @@ lvim.plugins = {
         -- Configuration here, or leave empty to use defaults
       })
     end
-  }
+  },
+  {
+    "mrjones2014/nvim-ts-rainbow",
+  },
+  -- lightspeed
+  {
+    "ggandor/lightspeed.nvim",
+    event = "BufRead",
+  },
 }
 
 local ok, copilot = pcall(require, "copilot")
@@ -44,6 +92,7 @@ end
 
 copilot.setup {
   suggestion = {
+    toggle_auto_trigger = true,
     keymap = {
       accept = "<c-l>",
       next = "<c-j>",
@@ -52,9 +101,6 @@ copilot.setup {
     },
   },
 }
-
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
 
 -- set trouble keymap
 lvim.builtin.which_key.mappings["t"] = {
@@ -66,3 +112,18 @@ lvim.builtin.which_key.mappings["t"] = {
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Diagnostics" },
 }
+
+lvim.builtin.which_key.mappings["S"] = {
+  name = "Session",
+  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+}
+
+lvim.builtin.treesitter.rainbow.enable = true
+-- format on save
+lvim.format_on_save = true
+lvim.reload_config_on_save = true
+
+vim.opt.wrap = true
+vim.opt.relativenumber = true
